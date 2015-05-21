092076d6cc8825d4ae2f1679dab0a51ecfd70fb4
Roger Frace <rfrace@nfusion.com>
2015-05-21 13:14:32 -0500

    real answers

--- php/answers.php
+++ php/answers.php
@@ -1,13 +1,39 @@
 <?php
 $allanswers = array(
 
-'yes',
-'no',
-'maybe',
-'for sure',
-'perhaps',
-'no way',
-'definitely'
+'Are you kidding?',
+'As I see it, yes.',
+'Ask again later.',
+'Better not tell you now.',
+'Cannot predict now.',
+'Concentrate and ask again.',
+'Definitely not.',
+'Don\'t bet on it.',
+'Don\'t count on it.',
+'Forget about it.',
+'Go for it!',
+'I have my doubts.',
+'It is certain.',
+'It is decidedly so.',
+'Looking good!',
+'Looks good to me!',
+'Most likely.',
+'My reply is no.',
+'My sources say no.',
+'Outlook good.',
+'Outlook not so good.',
+'Outlook so so.',
+'Probably.',
+'Reply hazy, try again.',
+'Signs point to yes.',
+'Very doubtful.',
+'Who knows?',
+'Without a doubt.',
+'Yes - definitely.',
+'Yes, in due time.',
+'Yes.',
+'You may rely on it.',
+'You will have to wait.'
 
 );
 ?>
